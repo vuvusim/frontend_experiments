3e99226656c0cf445f0e038010d2316f676f2cdb
Kestutis Januskevicius <infohata@gmail.com>
2022-10-27 09:19:44 +0300

    flask simple grid

--- flask_styling.py
+++ flask_styling.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("styling/home.html")
 
+@app.route("/containers")
+def containers():
+    return render_template("styling/containers.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
